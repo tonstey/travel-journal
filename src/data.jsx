1ded206b59adbc908d8ed9f225fc1c72c574be5f
tonstey <tonyvuong245@gmail.com>
2024-06-16 21:17:34 -0700

    commit 5

--- src/data.jsx
+++ src/data.jsx
@@ -6,7 +6,7 @@ export default [
       startDate: "12 Jan, 2021",
       endDate: "24 Jan, 2021",
       description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-      imageUrl: "/images/mountfuji.jpg"
+      imageUrl: "travel-journal/images/mountfuji.jpg"
   },
 
   {
